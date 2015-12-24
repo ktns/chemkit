d76286e16f67a546682e135317c90bd772935cae
Katsuhiko Nishimra <ktns.87@gmail.com>
2015-12-24 18:59:24 +0900

    Remove example files

--- debian/chemkit.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the chemkit package
-#
-0 4	* * *	root	[ -x /usr/bin/chemkit_maintenance ] && /usr/bin/chemkit_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(chemkit):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="chemkit" command="/usr/bin/chemkit"
